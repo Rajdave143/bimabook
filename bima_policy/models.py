156eb22a64aa500b449282a17269932cd2f7f679
Rajdave143 <69081842+Rajdave143@users.noreply.github.com>
2022-07-29 11:01:20 +0530

    Add existing project files to Git

--- bima_policy/models.py
+++ bima_policy/models.py
@@ -1,10 +1,21 @@
 from djongo import models
-
+# from django import forms
 # Create your models here.
 
-class login(models.Model):
-    id= models.CharField(max_length=100)
-    password = models.CharField(max_length=50)
-
-    class Meta:
-        abstract = True
+class bima_user(models.Model):
+    sr_no = models.IntegerField
+    login_id = models.CharField(max_length=50)
+    staffname = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=20)
+    cnfmpassword= models.CharField(max_length=20)
+    status= models.TextField   
+    # class Meta:
+    #     abstract = True
+        
+# class Bima_Form(forms.ModelForm):
+#     class Meta:
+#         model = bima_user
+#         fields = (
+#             'sr_no', 'login_id','staffname','username','password','cnfmpassword',''
+#         )
